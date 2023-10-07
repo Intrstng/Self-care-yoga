0b0b5cf338f8c0e070f0af5c6d080e317c54c4d1
unknown <intrstg@gmail.com>
2023-10-07 15:57:32 +0300

    feat: add information to FAQ section

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { showSlide, nextSlide, prevSlide, sliderBtnNext, sliderBtnPrev } from '.
 import  { prevCard, nextCard, showPrev, showNext  } from './js/Cards';
 import { scrollUp, trackScroll, scrollTop } from './js/ScrollTop';
 import { playVideoBtns, playVideo, stopVideo } from './js/VideoPlayer';
+import { galleryBtns, selectVideoByTag } from './js/VideoGallery';
 
 // Welcome section slider
 sliderBtnNext.addEventListener('click', nextSlide);
@@ -19,46 +20,26 @@ scrollUp.addEventListener('click', scrollTop);
 prevCard.addEventListener('click',showPrev);
 nextCard.addEventListener('click',showNext);
 
-// VideoPlayer
+// Video player
 playVideoBtns.forEach(item => item.addEventListener('click', (e) => playVideo(e)));
 window.addEventListener('click', stopVideo);
 
-// setInterval(() => {
-//   nextSlide()
-//   console.log('next')
-// }, 5000);
-
-
-
-
-const galleryBtns = document.querySelectorAll('.button-trial'),
-      videoGalleryItemsArr = document.querySelectorAll('.video__card');
-let selectedBtnTags = [];
-
+// Video gallery
 galleryBtns.forEach(item => {
   item.addEventListener('click', (e) => selectVideoByTag(e));
 });
 
 
 
-function selectVideoByTag(e) {
-  selectedBtnTags = e.currentTarget.firstElementChild.dataset.tag.split(', ');
-  
-  videoGalleryItemsArr.forEach(video => {
-    video.style.display = 'none';
-  })
 
-  videoGalleryItemsArr.forEach(video => {
-    if (selectedBtnTags.includes(video.dataset.type)) {
-      video.style.display = 'block';
-    }
-  })
-  changeBtnActiveState(e);
-}
 
-function changeBtnActiveState(e) {
-  galleryBtns.forEach(item => {
-    item.firstElementChild.classList.remove('btn_active');
-  });
-  e.currentTarget.firstElementChild.classList.add('btn_active');
-}
+
+
+
+
+
+
+// setInterval(() => {
+//   nextSlide()
+//   console.log('next')
+// }, 5000);
--- src/js/VideoGallery.js
+++ src/js/VideoGallery.js
@@ -1,8 +1,27 @@
-const galleryContainer = document.querySelector('.practices__buttons');
+const galleryBtns = document.querySelectorAll('.button-select'),
+      videoGalleryItemsArr = document.querySelectorAll('.video__card');
+let selectedBtnTags = [];
 
+function selectVideoByTag(e) {
+  selectedBtnTags = e.currentTarget.firstElementChild.dataset.tag.split(', ');
+  
+  videoGalleryItemsArr.forEach(video => {
+    video.style.display = 'none';
+  })
 
-galleryContainer.addEventListener('click', function(e) {
-  if (e.target.closest('btn')) {
-    console.log('ok')
-  }
-})
+  videoGalleryItemsArr.forEach(video => {
+    if (selectedBtnTags.includes(video.dataset.type)) {
+      video.style.display = 'block';
+    }
+  })
+  changeBtnActiveState(e);
+}
+
+function changeBtnActiveState(e) {
+  galleryBtns.forEach(item => {
+    item.firstElementChild.classList.remove('btn_active');
+  });
+  e.currentTarget.firstElementChild.classList.add('btn_active');
+}
+
+export { galleryBtns, selectVideoByTag }
